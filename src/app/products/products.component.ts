b06fd4c54d155ac70f82fbf49f7b54fb187b9374
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-05 15:15:30 +0100

    Dispaly List of products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  // products = Array<any>;
+  products = [
+    {id:1, name:"Computer", price:6500},
+    {id:2, name:"Printer", price:1200},
+    {id:3, name:"SmartPhone", price:1200},
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.products = [
+    //   {id:1, name:"Computer", price:6500},
+    //   {id:2, name:"Printer", price:1200},
+    //   {id:3, name:"SmartPhone", price:1200},
+    // ];
   }
 
 }
