175bf4a6e0a46d7d48d591ef56f04833c40d30bc
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2022-10-09 16:14:38 +0100

    Show list generated by loop for

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable, of, throwError} from "rxjs";
 import {Product} from "../model/product.model";
+import {UUID} from "angular2-uuid";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,16 @@ export class ProductService {
 
   constructor() {
     this.products = [
-      {id:1, name:"Computer", price:6500, promotion : true},
-      {id:2, name:"Printer", price:1200, promotion : false},
-      {id:3, name:"SmartPhone", price:1200, promotion : true},
+      {id:UUID.UUID(), name:"Computer", price:6500, promotion : true},
+      {id:UUID.UUID(), name:"Printer", price:1200, promotion : false},
+      {id:UUID.UUID(), name:"SmartPhone", price:1200, promotion : true},
     ];
+
+    for (let i = 0; i < 10; i++) {
+      this.products.push({id:UUID.UUID(), name:"Computer", price:6500, promotion : true});
+      this.products.push({id:UUID.UUID(), name:"Printer", price:1200, promotion : false});
+      this.products.push({id:UUID.UUID(), name:"SmartPhone", price:1200, promotion : true});
+    }
   }
 
   public getAllProducts() : Observable<Product[]>{
@@ -23,12 +30,12 @@ export class ProductService {
       return of(this.products);
   }
 
-  public deleteProduct(id : number) : Observable<boolean>{
+  public deleteProduct(id : string) : Observable<boolean>{
     this.products = this.products.filter(p=>p.id != id);
     return of(true);
   }
 
-  public setPromotion(id : number) : Observable<boolean>{
+  public setPromotion(id : string) : Observable<boolean>{
     let product = this.products.find(p=>p.id==id);
     if(product != undefined){
       product.promotion=!product.promotion;
